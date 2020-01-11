93ad1ed664592d88e013330eb70c373059bf20e1
Dmitry Tverdokhleb <tverdokhlebd@edi-n.com>
2020-01-11 16:25:46 +0200

    progress l 26

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a slice of string
@@ -25,4 +29,17 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 func main() {
-	cards := newDeck()
+	/*	cards := newDeck()
 
-	cards.print()
+		hand, remainingCards := deal(cards, 5)
+
+		hand.print()
+		remainingCards.print()
+	*/
+	cards := newDeck()
+	cards.saveToFile("my_cards")
 }
