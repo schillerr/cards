03507395d3705a204316b5ec1a472fbb80cc2641
Dmitry Tverdokhleb <tverdokhlebd@edi-n.com>
2020-01-13 16:55:53 +0200

    lesson 30

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -56,4 +58,15 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",") // Ace of Spades,Two of Spades,Three of Spades,
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 func main() {
 	/*	cards := newDeck()
 
-			hand, remainingCards := deal(cards, 5)
+				hand, remainingCards := deal(cards, 5)
 
-			hand.print()
-			remainingCards.print()
+				hand.print()
+				remainingCards.print()
 
-		cards := newDeck()
-		cards.saveToFile("my_cards")
+			cards := newDeck()
+			cards.saveToFile("my_cards")
+
+		cards := newDeckFromFile("my_cards")
+		cards.print()
 	*/
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
