cb0065d283848850e4681a6745c85d5ea8f57051
Dmitry Tverdokhleb <tverdokhlebd@edi-n.com>
2020-01-13 14:41:00 +0200

    progress level 28 'error handling'

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 func main() {
 	/*	cards := newDeck()
 
-		hand, remainingCards := deal(cards, 5)
+			hand, remainingCards := deal(cards, 5)
 
-		hand.print()
-		remainingCards.print()
+			hand.print()
+			remainingCards.print()
+
+		cards := newDeck()
+		cards.saveToFile("my_cards")
 	*/
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
